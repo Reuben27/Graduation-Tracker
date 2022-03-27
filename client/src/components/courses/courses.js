7854991e201cc4df7774419d5d84900c2840b5dd
Kamal <kamal.vaishnav@iitgn.ac.in>
2022-03-27 10:15:27 +0530

    2

--- client/src/components/courses/courses.js
+++ client/src/components/courses/courses.js
@@ -3,88 +3,71 @@ import React from "react";
 function Courses() {
   return (
     <div className="bg-gray-500 p-10">
-      <div className="flex justify-between max-w-4xl m-auto bg-slate-50 px-5 py-2 rounded-md">
+      <div className="flex justify-between max-w-4xl m-auto bg-gray-600 px-5 py-2 rounded-md">
         <button className="bg-green-500 hover:bg-green-700 m-3 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Add Courses
         </button>
-        <button className="bg-green-500 hover:bg-green-700 m-3 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Paste Data
+        <button className="bg-blue-500 hover:bg-blue-700 m-3 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Save
         </button>
       </div>
       <div class="fixTableHead overflow-y-auto h-36 bg-slate-100 max-w-4xl m-auto mt-5 rounded">
-    <table className=" border-collapse w-full">
-      <thead className=" sticky top-0 justify-around">
-        <tr className="bg-gray-600">
-          <th className="sticky p-2">Course ID</th>
-          <th className="sticky p-2">Course Name</th>
-          <th className="sticky p-2">Discipline</th>
-          <th className="sticky p-2">Credit</th>
-          
-        </tr>
-      </thead>
-  
-      <tbody>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-          
-          
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-        <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-          <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-        <tr>
-        <td>MA 101</td>
-          <td>Maths 3</td>
-          <td>CSE</td>
-          <td>4</td>
-        </tr>
-      </tbody>
-        
-    </table>
-  </div>
-      
+        <table className=" border-collapse w-full text-center">
+          <thead className=" sticky top-0 justify-around">
+            <tr className="bg-gray-600">
+              <th className="sticky p-2">Course ID</th>
+              <th className="sticky p-2">Course Name</th>
+              <th className="sticky p-2">Discipline</th>
+              <th className="sticky p-2">Credit</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>MA 101</td>
+              <td>Maths 3</td>
+              <td>CSE</td>
+              <td>4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
